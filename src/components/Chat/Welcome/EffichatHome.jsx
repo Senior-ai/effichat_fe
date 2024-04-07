3d58fa0a5a79febe27b12746c03a1e7739b58da8
Shon Aizenshtein <68730456+Senior-ai@users.noreply.github.com>
2024-04-07 13:53:40 +0300

    added

--- src/components/Chat/Welcome/EffichatHome.jsx
+++ src/components/Chat/Welcome/EffichatHome.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 
 export default function EffichatHome() {
-  return (
-    <div className='h-full w-full dark:bg-dark_dark_bg_4 select-none border-l dark:border-l-dark_border_1 border-b-[6px] border-b-blue-400'>
-        <div className='-mt-1.5 w-full h-full flex flex-col gap-y-8 items-center justify-center'>
-            <span>
-                {/* //TODO -  Make notes here */}
-            </span>
-            {/* Infos */}
-            <div className="mt-1 text-center spce-y-[12px]">
-                <h1 className='text-[32px] dark:text-dark_text_4 font-extralight'>
-                    EffiChat!
-                </h1>
-                <p className="text-sm dark:text-dark_text_3">
-                    Send and receive messages and keep being efficient with your work.
-                </p>
+    return (
+        <div className='h-full w-full dark:bg-dark_dark_bg_4 select-none border-l dark:border-l-dark_border_1 border-b-[6px] border-b-blue-400'>
+            <div className='-mt-1.5 w-full h-full flex flex-col gap-y-8 items-center justify-center'>
+                <span>
+                    {/* //TODO -  Make notes here */}
+                    <div className='bg-indigo-300 h-4 w-4 '>
+
+                    </div>
+                </span>
+                {/* Infos */}
+                <div className="mt-1 text-center spce-y-[12px]">
+                    <h1 className='text-[32px] dark:text-dark_text_4 font-extralight'>
+                        EffiChat!
+                    </h1>
+                    <p className="text-sm dark:text-dark_text_3">
+                        Send and receive messages and keep being efficient with your work.
+                    </p>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
